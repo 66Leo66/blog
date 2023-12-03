936e89aa23a4133313bdbc5b8d1079657ddfe051
Leo Li <leoli@6leo6.com>
2023-12-03 16:57:52 +0800

    Prefer local font

--- uno.config.ts
+++ uno.config.ts
@@ -1,31 +1,37 @@
-import { defineConfig, presetUno, presetAttributify, presetTypography  } from "unocss";
+import {
+  defineConfig,
+  presetUno,
+  presetAttributify,
+  presetTypography,
+} from "unocss";
 
 export default defineConfig({
-    presets: [
-        presetUno(),
-        presetAttributify(),
-        presetTypography({
-            cssExtend: {
-                'blockquote': {
-                    'border-left': '5px solid #777',
-                    'background-color': '#eee',
-                    'padding': '0 1rem',
-                    'margin-left': '0',
-                    'margin-right': '0',
-                },
-                'blockquote p': {
-                    'display': 'block',
-                    'margin-block-start': '1em',
-                    'margin-block-end': '1em',
-                    'margin-inline-start': '0px',
-                    'margin-inline-end': '0px',
-                }
-            }
-        })
-    ],
-    theme: {
-        fontFamily: {
-            sans: 'MiSans VF'
-        }
-    }
-})
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetTypography({
+      cssExtend: {
+        blockquote: {
+          "border-left": "5px solid #777",
+          "background-color": "#eee",
+          padding: "0 1rem",
+          "margin-left": "0",
+          "margin-right": "0",
+        },
+        "blockquote p": {
+          display: "block",
+          "margin-block-start": "1em",
+          "margin-block-end": "1em",
+          "margin-inline-start": "0px",
+          "margin-inline-end": "0px",
+        },
+      },
+    }),
+  ],
+  theme: {
+    fontFamily: {
+      zh: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Noto Sans", Roboto, Oxygen, Cantarell, "Fira Sans", "Liberation Sans", "Droid Sans", Ubuntu, "PingFang SC", "HarmonyOS Sans SC", MiSans, "Hiragino Sans GB", "WenQuanYi Micro Hei", "Noto Sans CJK SC", "Noto Sans SC", "Microsoft YaHei", "Helvetica Neue", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+      en: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Noto Sans", Roboto, Oxygen, Cantarell, "Fira Sans", "Liberation Sans", "Droid Sans", Ubuntu, "Helvetica Neue", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    },
+  },
+});
