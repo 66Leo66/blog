41774ec9d029370259af6a334230ad1dbdad19bd
Leo Li <33322229+66Leo66@users.noreply.github.com>
2023-12-04 05:24:33 +0000

    Add sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-
 import markdoc from "@astrojs/markdoc";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://blog.6leo6.com",
   integrations: [UnoCSS({
     injectReset: true
-  }), markdoc()]
+  }), markdoc(), sitemap()]
 });
