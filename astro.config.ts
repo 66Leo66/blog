e4e3b62efb6214d4064a344e43e8c905fefff0b2
Leo Li <leoli@6leo6.com>
2024-10-13 19:10:41 +0800

    setup expressive-coed and move proxmox post

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,28 @@
-import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro';
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
 
 import mdx from "@astrojs/mdx";
+
+import expressiveCode from "astro-expressive-code";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.6leo6.com",
-  integrations: [UnoCSS({
-    injectReset: true
-  }), mdx()]
+  integrations: [
+    UnoCSS({
+      injectReset: true,
+    }),
+    expressiveCode({
+      plugins: [pluginLineNumbers()],
+      defaultProps: {
+        wrap: true,
+        overridesByLang: {
+          "bash,ps,sh": { preserveIndent: false },
+        },
+      },
+    }),
+    mdx(),
+  ],
+  markdown: {},
 });
