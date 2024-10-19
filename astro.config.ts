23bcba88357db435fa91e731a6de3d763f6511ca
Leo Li <leoli@6leo6.com>
2024-10-19 20:49:37 +0800

    astro 5 beta, ts alias, solid, pagefind

--- astro.config.ts
+++ astro.config.ts
@@ -6,10 +6,15 @@ import mdx from "@astrojs/mdx";
 import expressiveCode from "astro-expressive-code";
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
+import pagefind from "astro-pagefind";
+
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.6leo6.com",
   integrations: [
+    solidJs(),
     UnoCSS({
       injectReset: true,
     }),
@@ -23,6 +28,7 @@ export default defineConfig({
       },
     }),
     mdx(),
+    pagefind(),
   ],
   markdown: {},
 });
